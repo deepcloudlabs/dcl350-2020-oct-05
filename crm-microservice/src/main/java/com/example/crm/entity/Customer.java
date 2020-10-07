545f654ba0057cfdf970060543d609f2766c4026
deepcloudlabs <deepcloudlabs@gmail.com>
2020-10-07 09:40:05 +0300

    add setter/getter, equals/hashCode, toString and default constructor

--- crm-microservice/src/main/java/com/example/crm/entity/Customer.java
+++ crm-microservice/src/main/java/com/example/crm/entity/Customer.java
@@ -3,23 +3,125 @@ package com.example.crm.entity;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.Id;
+import javax.persistence.Lob;
 import javax.persistence.Table;
 
 @Entity
-@Table(name="customers")
+@Table(name = "customers")
 public class Customer {
 	@Id
 	private String identity;
-	@Column(name= "full_name")
+	@Column(name = "full_name")
 	private String fullname;
-	@Column(name= "home_address")
+	@Column(name = "home_address")
 	private String homeAddress;
-	@Column(name= "business_address")
+	@Column(name = "business_address")
 	private String businessAddress;
 	private String email;
 	private String sms;
-	@Column(name= "birth_year")
+	@Column(name = "birth_year")
 	private int birthYear;
+	@Lob
+	@Column(columnDefinition = "longblob")
 	private byte[] photo;
-	
+
+	public Customer() {
+	}
+
+	public String getIdentity() {
+		return identity;
+	}
+
+	public void setIdentity(String identity) {
+		this.identity = identity;
+	}
+
+	public String getFullname() {
+		return fullname;
+	}
+
+	public void setFullname(String fullname) {
+		this.fullname = fullname;
+	}
+
+	public String getHomeAddress() {
+		return homeAddress;
+	}
+
+	public void setHomeAddress(String homeAddress) {
+		this.homeAddress = homeAddress;
+	}
+
+	public String getBusinessAddress() {
+		return businessAddress;
+	}
+
+	public void setBusinessAddress(String businessAddress) {
+		this.businessAddress = businessAddress;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getSms() {
+		return sms;
+	}
+
+	public void setSms(String sms) {
+		this.sms = sms;
+	}
+
+	public int getBirthYear() {
+		return birthYear;
+	}
+
+	public void setBirthYear(int birthYear) {
+		this.birthYear = birthYear;
+	}
+
+	public byte[] getPhoto() {
+		return photo;
+	}
+
+	public void setPhoto(byte[] photo) {
+		this.photo = photo;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((identity == null) ? 0 : identity.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Customer other = (Customer) obj;
+		if (identity == null) {
+			if (other.identity != null)
+				return false;
+		} else if (!identity.equals(other.identity))
+			return false;
+		return true;
+	}
+
+	@Override
+	public String toString() {
+		return "Customer [identity=" + identity + ", fullname=" + fullname + ", homeAddress=" + homeAddress
+				+ ", businessAddress=" + businessAddress + ", email=" + email + ", sms=" + sms + ", birthYear="
+				+ birthYear + "]";
+	}
+
 }
