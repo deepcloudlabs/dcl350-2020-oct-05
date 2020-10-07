ce7a5857182af87724f0831b508880654c089260
deepcloudlabs <deepcloudlabs@gmail.com>
2020-10-07 14:18:05 +0300

    add bean validation language files

--- crm-microservice/src/main/java/com/example/crm/CrmMicroserviceApplication.java
+++ crm-microservice/src/main/java/com/example/crm/CrmMicroserviceApplication.java
@@ -3,6 +3,7 @@ package com.example.crm;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+// -Duser.language=tr -Duser.country=TR
 @SpringBootApplication
 public class CrmMicroserviceApplication {
 
