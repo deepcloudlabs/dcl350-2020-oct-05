580d63272d932053591ecd9f10854489356e009d
deepcloudlabs <deepcloudlabs@gmail.com>
2020-10-07 16:41:01 +0300

    add post request

--- lottery-microservice/src/main/java/com/example/lottery/LotteryMicroserviceApplication.java
+++ lottery-microservice/src/main/java/com/example/lottery/LotteryMicroserviceApplication.java
@@ -3,6 +3,7 @@ package com.example.lottery;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+// curl localhost:8300/lottery/api/v1/actuator/refresh -X POST -d {} -H "Content-Type: application/json"
 @SpringBootApplication
 public class LotteryMicroserviceApplication {
 
