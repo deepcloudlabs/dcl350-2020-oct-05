8bef73ac196df10eb3c8343ee9f52073634d4164
deepcloudlabs <deepcloudlabs@gmail.com>
2020-10-08 11:53:22 +0300

    add eureka client

--- lottery-microservice/src/main/java/com/example/lottery/LotteryMicroserviceApplication.java
+++ lottery-microservice/src/main/java/com/example/lottery/LotteryMicroserviceApplication.java
@@ -2,9 +2,13 @@ package com.example.lottery;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 // curl localhost:8300/lottery/api/v1/actuator/refresh -X POST -d {} -H "Content-Type: application/json"
 @SpringBootApplication
+@EnableEurekaClient
+@EnableDiscoveryClient
 public class LotteryMicroserviceApplication {
 
 	public static void main(String[] args) {
