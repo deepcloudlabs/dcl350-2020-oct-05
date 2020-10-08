d9d2b6a3e1606466816c103a3a5619d5640e2ec9
deepcloudlabs <deepcloudlabs@gmail.com>
2020-10-08 14:47:41 +0300

    initial commit

--- lottery-client-spring-boot/src/main/java/com/example/lottery/LotteryClientSpringBootApplication.java
+++ lottery-client-spring-boot/src/main/java/com/example/lottery/LotteryClientSpringBootApplication.java
@@ -4,6 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 // Arguments -> VM arguments
@@ -12,6 +13,7 @@ import org.springframework.scheduling.annotation.EnableScheduling;
 @EnableScheduling
 @EnableEurekaClient
 @EnableDiscoveryClient
+@EnableFeignClients
 public class LotteryClientSpringBootApplication {
 
 	public static void main(String[] args) {
