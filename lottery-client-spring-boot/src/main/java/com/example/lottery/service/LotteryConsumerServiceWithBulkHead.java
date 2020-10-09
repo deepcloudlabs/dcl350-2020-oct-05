bb70238392ae532acbd8089b376e90475f966e0c
deepcloudlabs <deepcloudlabs@gmail.com>
2020-10-09 15:37:50 +0300

    add bulk head

--- lottery-client-spring-boot/src/main/java/com/example/lottery/service/LotteryConsumerServiceWithBulkHead.java
+++ lottery-client-spring-boot/src/main/java/com/example/lottery/service/LotteryConsumerServiceWithBulkHead.java
@@ -1,5 +1,26 @@
 package com.example.lottery.service;
 
-public class LotteryConsumerServiceWithBulkHead {
+import java.net.SocketException;
 
+import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
+
+import io.github.resilience4j.bulkhead.annotation.Bulkhead;
+import io.github.resilience4j.bulkhead.annotation.Bulkhead.Type;
+
+@Service
+public class LotteryConsumerServiceWithBulkHead {
+	
+	@Bulkhead(name="lotterySrvBulkHead", fallbackMethod = "getNumbersFallback",
+			   type=Type.THREADPOOL)
+	public String getNumbers() {
+		RestTemplate rt = new RestTemplate();
+		return rt.getForEntity("http://localhost:8300/lottery/api/v1/numbers?n=10", String.class).getBody();
+	}
+
+	public String getNumbersFallback(Exception e) {
+		if (e instanceof SocketException)
+			return "[[1,2,3,4,5,6]]";
+		return "[[4,8,15,16,23,42]]";
+	}
 }
