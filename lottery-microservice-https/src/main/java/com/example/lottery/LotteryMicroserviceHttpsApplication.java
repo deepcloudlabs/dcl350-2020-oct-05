4bdd5cf7e1c6d72e24035301bb00bc2affff2ebe
deepcloudlabs <deepcloudlabs@gmail.com>
2020-10-09 16:59:18 +0300

    add config

---
+++ lottery-microservice-https/src/main/java/com/example/lottery/LotteryMicroserviceHttpsApplication.java
@@ -0,0 +1,13 @@
+package com.example.lottery;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LotteryMicroserviceHttpsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LotteryMicroserviceHttpsApplication.class, args);
+	}
+
+}
